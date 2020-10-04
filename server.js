ef438750a60d89330d821580f6ed14e30a61cb1b
Aramosgr <aramosgr@gmail.com>
2020-10-04 11:32:42 +0100

    fixing the GOD DAMM Cors

--- server.js
+++ server.js
@@ -10,8 +10,9 @@ app.use(bodyParser.json());
 
 
 app.get('/', (request, response) => {
-    response.header("Access-Control-Allow-Origin", "https://halloween.azurewebsites.net/"); // update to match the domain you will make the request from
-    response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    response.header("Access-Control-Allow-Origin", "*");
+    response.header("Access-Control-Allow-Methods", "*");
+    response.header("Access-Control-Allow-Headers", "*");
     response.json({ service: process.env.npm_package_name, version: process.env.npm_package_version });
 });
 
